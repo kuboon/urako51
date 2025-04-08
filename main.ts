a78abd3588d87a8f7a200278ae9d4a58b098f98e
Ohkubo KOHEI <o@kbn.one>
2025-04-08 14:50:00 +0900

    export-handler

--- main.ts
+++ main.ts
@@ -6,8 +6,9 @@
 
 import "$std/dotenv/load.ts";
 
-import { start } from "$fresh/server.ts";
+import { createHandler } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
-await start(manifest, config);
+const handler = createHandler(manifest, config);
+export default { fetch: handler };
