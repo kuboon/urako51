32579c332161aa8b3e48d253804d368245b5631f
Ohkubo KOHEI <o@kbn.one>
2025-09-11 17:45:40 +0900

    use deno deploy ea

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,7 +7,6 @@ export default define.page(function App({ Component }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{data.title}</title>
-        <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
         <Component />
