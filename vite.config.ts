dd2a51556905f624183592e072bda512940840b8
Ohkubo KOHEI <o@kbn.one>
2025-09-20 08:27:29 +0000

    deno update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
-import "@std/dotenv/load";
 
 export default defineConfig({
   plugins: [
